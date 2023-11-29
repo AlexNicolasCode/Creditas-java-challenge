97c68abd3707a29e6eb19a5f6d17b980bba57f00
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-29 17:25:42 -0300

    test: available loans should returns guarantee loan when customer location is SP

--- src/test/java/LoanMatcherTest.java
+++ src/test/java/LoanMatcherTest.java
@@ -8,7 +8,7 @@ class LoanMatcherTest {
 
     @Test
     void returnsPersonalLoanWhenCustomerIncomeIsUnder3000() {
-        Customer customer = new Customer(30, 3000);
+        Customer customer = new Customer("PB", 30, 3000);
         LoanMatcher loanMatcher = new LoanMatcher(customer);
         List<Loan> availableLoans = loanMatcher.loans();
 
@@ -20,7 +20,7 @@ class LoanMatcherTest {
 
     @Test
     void returnsPersonalLoanWhenCustomerIncomeIsBetween3000And5000() {
-        Customer customer = new Customer(30, 4000);
+        Customer customer = new Customer("PB", 30, 4000);
         LoanMatcher loanMatcher = new LoanMatcher(customer);
         List<Loan> availableLoans = loanMatcher.loans();
 
@@ -32,7 +32,7 @@ class LoanMatcherTest {
 
     @Test
     void returnsPersonalLoanWhenCustomerIncomeIsMoreThan5000() {
-        Customer customer = new Customer(30, 6000);
+        Customer customer = new Customer("PB", 30, 6000);
         LoanMatcher loanMatcher = new LoanMatcher(customer);
         List<Loan> availableLoans = loanMatcher.loans();
 
@@ -44,7 +44,19 @@ class LoanMatcherTest {
 
     @Test
     void returnsGuaranteeLoanWhenCustomerAgeIsLessThan30() {
-        Customer customer = new Customer(29, 0);
+        Customer customer = new Customer("PB", 29, 0);
+        LoanMatcher loanMatcher = new LoanMatcher(customer);
+        List<Loan> availableLoans = loanMatcher.loans();
+
+        assertThat(availableLoans.size()).isEqualTo(2);
+
+        assertThat(availableLoans.stream().findFirst().isPresent()).isTrue();
+        assertThat(availableLoans.get(0).type()).isEqualTo("GUARANTEE_LOAN");
+    }
+
+    @Test
+    void returnsGuaranteeLoanWhenCustomerLocationIsSP() {
+        Customer customer = new Customer("SP", 31, 0);
         LoanMatcher loanMatcher = new LoanMatcher(customer);
         List<Loan> availableLoans = loanMatcher.loans();
 
