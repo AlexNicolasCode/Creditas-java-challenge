667782ca8308f0f5e09a64f3a65cb9050496817c
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-29 18:03:05 -0300

    feat: Loan object should return tax according to selected type

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -8,4 +8,15 @@ public class Loan {
     public String type() {
         return type;
     }
+
+    public int tax() {
+        switch (type) {
+            case "GUARANTEE_LOAN":
+                return 3;
+            case "CONSIGNED_LOAN":
+                return 2;
+            default:
+                return 4;
+        }
+    }
 }
